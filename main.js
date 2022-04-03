1732d0c1366490a2c25e5fbf5d45dd73887e0cc9
Thiago Sev <thiagosev@Thiago-S1.local>
2022-04-03 19:57:38 -0400

    move repo

---
+++ main.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const BotManager = require('./bots/bot-manager.js');
+
+// Initialize Discord Bot
+const client = new Discord.Client();
+const bot = new BotManager();
+
+client.on('ready', () => {
+    bot.handleStart(client);
+});
+
+client.on('message', (message) => {
+    bot.handleMessage(message, client);
+});
+
+client.login(bot.authToken);
